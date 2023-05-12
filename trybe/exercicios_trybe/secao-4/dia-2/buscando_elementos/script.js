662f713f5178fd593240b771ace14c7bcee7eef0
Kleber Rodrigues <kleber-ar@hotmail.com>
2023-05-12 16:24:07 -0300

    att JS

---
+++ trybe/exercicios_trybe/secao-4/dia-2/buscando_elementos/script.js
@@ -0,0 +1,11 @@
+// acessar o elemento "where-are-you"
+
+const whereAreYou = document.querySelector('where-are-you');
+
+console.log(whereAreYou);
+
+
+
+// Acesse parent a partir de where-are-you e adicione uma color a ele.
+
+
