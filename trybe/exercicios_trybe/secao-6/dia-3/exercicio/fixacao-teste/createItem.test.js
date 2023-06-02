090c0fdaea7d084003e69508442ca9098d7e88ad
kleber-ar <kleber-ar@hotmail.com>
2023-06-02 16:36:48 -0300

    att js

---
+++ trybe/exercicios_trybe/secao-6/dia-3/exercicio/fixacao-teste/createItem.test.js
@@ -0,0 +1,27 @@
+const createItem = require('./createItem');
+
+describe('a função createItem', () => {
+  it('cria um item válido', () => {
+    expect(createItem('banana','kg',1.99,20)).toEqual({name: 'banana',unit: 'kg', price: 1.99, quantity: 20});
+
+  });
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana','kg',1.99)).toHaveProperty('quantity',0);
+
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(2,'kg')).toThrow();
+  });
+  it('Lança um erro se o preço é negativo', () => {
+    expect(() => createItem('banana','kg',-1.99)).toThrow();
+
+  });
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana','kg', 0)).toThrow();
+
+  });
+});
